73bfe53cd5831456348c508a0f66c59907fcd4cd
Padraic Cunningham <pcunningham80@gmail.com>
2023-05-15 10:12:18 +0100

    remove missing metric

--- receiver/vcenterreceiver/metrics.go
+++ receiver/vcenterreceiver/metrics.go
@@ -247,9 +247,6 @@ func (v *vcenterMetricScraper) processVMPerformanceMetrics(info *perfSampleResul
 					v.mb.RecordVcenterVMDiskLatencyAvgDataPoint(pcommon.NewTimestampFromTime(si.Timestamp), nestedValue, metadata.AttributeDiskDirectionWrite, metadata.AttributeDiskTypeVirtual)
 				case "disk.maxTotalLatency.latest":
 					v.mb.RecordVcenterVMDiskLatencyMaxDataPoint(pcommon.NewTimestampFromTime(si.Timestamp), nestedValue)
-				case "virtualDisk.totalReadLatency.maximum":
-					v.mb.RecordVcenterVMDiskLatencyAvgDataPoint(pcommon.NewTimestampFromTime(si.Timestamp), nestedValue, metadata.AttributeDiskDirectionRead, metadata.AttributeDiskTypeVirtual)
-
 				}
 			}
 		}
